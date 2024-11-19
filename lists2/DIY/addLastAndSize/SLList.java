c64facee7a898050495951e7358ec7b12252d42a
satelesport <152187916+satelesport@users.noreply.github.com>
2024-11-19 21:33:42 +0800

    rewrite the SSList

--- lists2/DIY/addLastAndSize/SLList.java
+++ lists2/DIY/addLastAndSize/SLList.java
@@ -1,5 +1,5 @@
 public class SLList {
-    public class IntNode {
+    public static class IntNode {
         public int item;
         public IntNode next;
         public IntNode(int i, IntNode n) {
@@ -26,12 +26,22 @@ public class SLList {
 
     /** Adds an item to the end of the list. */
     public void addLast(int x) {
-        /* Your Code Here! */
+        IntNode p=this.first;
+        while(p.next!=null){
+            p=p.next;
+        }
+        p.next=new IntNode(x,null);
     }
 
     /** Returns the number of items in the list using recursion. */
     public int size() {
-        /* Your Code Here! */
+        int cnt=0;
+        IntNode p=first;
+        while(p!=null){
+            cnt++;
+            p=p.next;
+        }
+        return cnt;
     }
 
 }
