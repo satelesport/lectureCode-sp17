cf4fef3e4447d6212e5eff3609e9a28b50c02317
satelesport <152187916+satelesport@users.noreply.github.com>
2024-11-20 14:58:15 +0800

    modify AList

--- lists4/DIY/AList.java
+++ lists4/DIY/AList.java
@@ -4,30 +4,37 @@
 
 public class AList {
     /** Creates an empty list. */
+    private int size;
+    private int[] list=new int[2000005];
     public AList() {
+        size=0;
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        list[size]=x;
+        size++;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return list[size-1];
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return list[i];
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        int x=list[size-1];
+        size--;
+        return x;
     }
 } 
